485aaca58dc2e9bdf1bb60cd8054a3f34cfc26ec
Kelvin Liang <kelvin9877@gmail.com>
2019-10-19 21:45:39 +0800

    build example page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'page/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   
+  root to: 'page#index'
 end
